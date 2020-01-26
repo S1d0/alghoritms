2c58b8effa6e294eb9c01171b6c9b37b3dadec66
Sebastian <idzkowski.s@gmail.com>
2020-01-26 23:32:34 +0100

    First task begin

---
+++ src/main/java/pl/my/alghoritms/examples/alghoritms/Lines.java
@@ -0,0 +1,8 @@
+package pl.my.alghoritms.examples.alghoritms;
+
+public class Lines {
+
+    void findCrossingLines() {
+
+    }
+}
--- src/main/java/pl/my/alghoritms/examples/alghoritms/SearchNDestroy.java
+++
@@ -1,7 +0,0 @@
-package pl.my.alghoritms.examples.alghoritms;
-
-public class SearchNDestroy {
-    void speak(){
-        System.out.println("Hi!");
-    }
-}
